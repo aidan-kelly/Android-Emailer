0218ec757b9f2b0190ba8adec106437b701d7654
Aidan Kelly <it.is.aidan@gmail.com>
2018-11-26 20:17:00 -0700

    Added ability to sign in and create an email

--- app/src/main/java/net/aidanjameskelly/emailtest/MainActivity.java
+++ app/src/main/java/net/aidanjameskelly/emailtest/MainActivity.java
@@ -1,19 +1,34 @@
 package net.aidanjameskelly.emailtest;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button send;
+    TextView senderTextView;
+    TextView passwordTextView;
+    TextView recipientTextView;
+    TextView subjectTextView;
+    TextView bodyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        senderTextView = findViewById(R.id.senderTextView);
+        passwordTextView = findViewById(R.id.passwordTextView);
+        recipientTextView = findViewById(R.id.recipientTextView);
+        subjectTextView = findViewById(R.id.subjectTextView);
+        bodyTextView = findViewById(R.id.emailTextView);
+
+
     }
 
     public void sendEmail(View view){
@@ -22,16 +37,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    GMailSender sender = new GMailSender("SENDER_EMAIL",
-                            "EMAIL_PASSWORD");
-                    sender.sendMail("Hello from JavaMail", "Body from JavaMail",
-                            "SENDER_EMAIL", "RECEIVER_EMAIL");
+                    GMailSender sender = new GMailSender(senderTextView.getText().toString(),
+                            passwordTextView.getText().toString());
+                    sender.sendMail(subjectTextView.getText().toString(), bodyTextView.getText().toString(),
+                            senderTextView.getText().toString(), recipientTextView.getText().toString());
                 } catch (Exception e) {
                     Log.e("SendMail", e.getMessage(), e);
                 }
             }
 
+
+
         }).start();
+        Toast.makeText(this, "Email sent!", Toast.LENGTH_SHORT).show();
     }
 
 
